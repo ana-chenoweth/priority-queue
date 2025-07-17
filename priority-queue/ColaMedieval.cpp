702b39f935fb727349ef5ebe5aa6ef59c7b61dbd
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-17 10:28:15 -0400

    Class priority queue: Cola Medieval

---
+++ priority-queue/ColaMedieval.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "ColaMedieval.hpp"
+
+//**********************************************
+ColaMedieval::ColaMedieval() : tam(0), tamNoble(0), tamPlebeyo(0), fondo(nullptr), fondoNoble(nullptr)
+{
+}
---
+++ priority-queue/ColaMedieval.hpp
@@ -0,0 +1,31 @@
+#ifndef ColaMedievalMEDIEVAL_HPP_INCLUDED
+#define ColaMedievalMEDIEVAL_HPP_INCLUDED
+
+    class ColaVacia : public std::exception{
+    public:
+        virtual const char * what() const throw();
+     };
+class ColaMedieval
+{
+public:
+
+    ColaMedieval();
+
+private:
+    int tam;
+    int tamNoble;
+    int tamPlebeyo;
+    struct Individuo
+    {
+        std::string nombre;
+        std::string claseSocial;
+        Individuo *siguiente;
+
+        Individuo(std::string nom, std::string cs, Individuo *sig) : nombre(nom), claseSocial(cs), siguiente(sig) {}
+    };
+
+    Individuo *fondo;
+    Individuo *fondoNoble;
+};
+
+#endif // ColaMedievalMEDIEVAL_HPP_INCLUDED
